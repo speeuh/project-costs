fde08afecad3c498595eb024b9de35f2b0097a66
speeuh <lu.alves306@hotmail.com>
2022-04-28 00:45:01 -0300

    Adding post and remove path

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,13 +1,40 @@
+import { useHistory } from 'react-router-dom'
+import { stringify } from 'uuid'
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject() {
 
+    const history = useHistory()
+
+    function createPost(project) {
+
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            // redirect
+            history.push("/projects", {message: "Project created successfully"})
+        })
+        .catch(err => console.log(err))
+
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Create Project</h1>
             <p>Create your project to add the services</p>
-            <ProjectForm btnText="Create Project" />
+            <ProjectForm handleSubmit={createPost} btnText="Create Project" />
         </div>    
     )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,82 @@
-function Projects() {
+import { useLocation } from "react-router-dom";
 
-    return <h1>Projects</h1>
+import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+import Loading from "../layout/Loading";
+
+import styles from "./Projects.module.css";
+import { useState, useEffect } from "react";
+
+function Projects() {
+  const [removeLoading, setRemoveLoading] = useState(false);
+  const [projects, setProjects] = useState([]);
+  const [projectMessage, setProjectMessage] = useState("");
+
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+        setRemoveLoading(true);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  function removeProject(id) {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(projects.filter((project) => project.id !== id));
+        setProjectMessage("Project removed successfully");
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>My Projects</h1>
+        <LinkButton to="/newProject" text="Create Project" />
+      </div>
+      {message && <Message type="success" msg={message} />}
+      {projectMessage && <Message type="success" msg={projectMessage} />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+              handleRemove={removeProject}
+            />
+          ))}
+        {!removeLoading && <Loading />}
+        {removeLoading && projects.length === 0 && (
+          <p>There are no projects registered!</p>
+        )}
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
